ca90d481d798499541912a1fab030e9e6ee96992
stutib-crest <stutib@splunk.com>
2024-08-01 18:28:47 +0530

    dev check changes

--- googlesheets_connector.py
+++ googlesheets_connector.py
@@ -1,8 +1,17 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-# -----------------------------------------
-# Phantom sample App Connector python file
-# -----------------------------------------
+# File: googlesheets_connector.py
+
+# Copyright (c) Splunk, 2024
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions
+# and limitations under the License.
 
 # Python 3 Compatibility imports
 from __future__ import print_function, unicode_literals
@@ -71,7 +80,7 @@ class GoogleSheetsConnector(BaseConnector):
             self.save_progress(TEST_CONNECTIVITY_FAILED)
             return action_result.get_status()
 
-        service.projects().serviceAccounts().get(name=PROJECT_NAME_PATH + self._service_email).execute()
+        _ = service.projects().serviceAccounts().get(name=PROJECT_NAME_PATH + self._service_email).execute()
         # Return success
         self.save_progress(TEST_CONNECTIVITY_PASSED)
         return action_result.set_status(phantom.APP_SUCCESS)
@@ -92,7 +101,7 @@ class GoogleSheetsConnector(BaseConnector):
         if phantom.is_fail(ret_val):
             return action_result.get_status()
 
-        service.spreadsheets()
+        _ = service.spreadsheets()
         try:
             response = service.spreadsheets().values().append(
                 spreadsheetId=sheet_id,
